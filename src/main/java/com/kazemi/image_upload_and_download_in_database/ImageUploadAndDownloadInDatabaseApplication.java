94ca3575d07e5ce89be7d0f4883b597e449217f2
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-06 15:21:39 +0200

    Create project.

---
+++ src/main/java/com/kazemi/image_upload_and_download_in_database/ImageUploadAndDownloadInDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.kazemi.image_upload_and_download_in_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUploadAndDownloadInDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageUploadAndDownloadInDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kazemi/image_upload_and_download_in_database/ImageUploadAndDownloadInDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazemi.image_upload_and_download_in_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageUploadAndDownloadInDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
